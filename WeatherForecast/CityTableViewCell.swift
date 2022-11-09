ec4f9c6da608803622641c2b8980ef50262c256d
ArtyomPotapov <artyom.potapov@icloud.com>
2022-11-09 18:54:47 +0300

    Настройка динамической ячейки первого vc

---
+++ WeatherForecast/CityTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  CityTableViewCell.swift
+//  WeatherForecast
+//
+//  Created by Artyom Potapov on 09.11.2022.
+//
+
+import UIKit
+
+class CityTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ WeatherForecast/DetailViewController.swift
@@ -0,0 +1,21 @@
+//
+//  DetailViewController.swift
+//  WeatherForecast
+//
+//  Created by Artyom Potapov on 09.11.2022.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+
+   
+
+}
