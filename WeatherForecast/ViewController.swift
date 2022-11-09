ca95363628fe1973bfa2596fb2fc9c8ec8473f6a
ArtyomPotapov <artyom.potapov@icloud.com>
2022-11-09 17:08:56 +0300

    initial

--- WeatherForecast/ViewController.swift
+++ WeatherForecast/ViewController.swift
@@ -6,14 +6,19 @@
 //
 
 import UIKit
+let urlString = "55.793080, 37.544226"
+
+let keyAPI = "X-Yandex-API-Key"
+let keyAPIValue = "7feb12d1-5fb4-41eb-afba-b02d43f017d2"
 
 class ViewController: UIViewController {
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
 
-
+    
 }
 
